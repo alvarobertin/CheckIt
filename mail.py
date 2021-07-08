11f16568255e19525689bab2c652f7e180b1c217
alvarobertin <50723409+alvarobertin@users.noreply.github.com>
2021-07-07 21:57:05 -0500

    update

--- mail.py
+++ mail.py
@@ -1,31 +1,32 @@
 import smtplib, ssl
 
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
+def send():
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
 
-keys = open("keys.private", "r")
-lines = keys.readlines()
-sender_email, password, receiver_email = lines[0].strip().split()
-message = """\
-Subject: Hi there
+    keys = open("keys.private", "r")
+    lines = keys.readlines()
+    sender_email, password, receiver_email = lines[0].strip().split()
+    message = """\
+    Subject: NEW PC IN STOCK
 
-This message is sent from Python."""
+    NEW PC IN STOCK: https://www.falabella.com.co/falabella-co/category/cat4920940/Computadores-Gaming """
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+    # Create a secure SSL context
+    context = ssl.create_default_context()
 
-# Try to log in to server and send email
-try:
-        server = smtplib.SMTP("smtp.gmail.com")
-        server.connect(smtp_server, port)
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+    # Try to log in to server and send email
+    try:
+            server = smtplib.SMTP("smtp.gmail.com")
+            server.connect(smtp_server, port)
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit()
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    finally:
+        server.quit()
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-#$$("#search_numResults")[0].textContent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+import mail
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.falabella.com.co/falabella-co/category/cat4920940/Computadores-Gaming")
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    idElement = 'testId-ProductLandingContainer-totalResults'
+    textChange = "Resultados (8)"
 
+    try:
+        driver.get("https://www.falabella.com.co/falabella-co/category/cat4920940/Computadores-Gaming")
 
+        element = driver.find_element_by_id(idElement)
 
-try:
-    element = driver.find_element_by_id('testId-ProductLandingContainer-totalResults')
-    print(element.text)
-except Exception as e:
-    print(e)
+        print(f'Tracked id: {idElement} ')
+        print(f'Tracked Text: {element.text} ')
+
+    except Exception as e:
+        print(f'An Error has occurred: \n {e} ')
+    else:
+        # If there is a change in the object
+        if element.text != textChange:
+            print("Good News")
+            #mail.send()
+        else:
+            print("Bad News")
+    finally:
+        print("Program ending")
+
+main()
